9f22dfdc5fff593b9970eba31c2f6a475e8d1f0c
FloSoft <sf-team@siedler25.org>
2011-11-25 08:46:51 +0100

    update for 1.0.0

--- src/mod_EditableSign.java
+++ src/mod_EditableSign.java
@@ -3,15 +3,21 @@ package net.minecraft.src;
 public class mod_EditableSign extends BaseMod {
 
 	public mod_EditableSign() {
+	}
+
+	@Override
+	public void load() {
 		Block.blocksList[63] = null;
-		Block signPost = (new BlockEditableSign(63, net.minecraft.src.TileEntitySign.class, true)).setHardness(1.0F).setStepSound(Block.soundWoodFootstep).setBlockName("sign").disableStats().setRequiresSelfNotify();
+		Block signPost = (new BlockEditableSign(63, net.minecraft.src.TileEntitySign.class, true)).setHardness(1.0F)
+				.setStepSound(Block.soundWoodFootstep).setBlockName("sign").disableStats().setRequiresSelfNotify();
 
 		Block.blocksList[68] = null;
-		Block signWall = (new BlockEditableSign(68, net.minecraft.src.TileEntitySign.class, false)).setHardness(1.0F).setStepSound(Block.soundWoodFootstep).setBlockName("sign").disableStats().setRequiresSelfNotify();
-		
+		Block signWall = (new BlockEditableSign(68, net.minecraft.src.TileEntitySign.class, false)).setHardness(1.0F)
+				.setStepSound(Block.soundWoodFootstep).setBlockName("sign").disableStats().setRequiresSelfNotify();
+
 		try {
-			ModLoader.setPrivateValue(Block.class, null, "aE", signPost);
-			ModLoader.setPrivateValue(Block.class, null, "aJ", signWall);
+			ModLoader.setPrivateValue(Block.class, null, "aD", signPost);
+			ModLoader.setPrivateValue(Block.class, null, "aI", signWall);
 		} catch (Exception e) {
 			e.printStackTrace();
 			try {
@@ -23,7 +29,8 @@ public class mod_EditableSign extends BaseMod {
 		}
 	}
 
-	public String Version() {
-		return "1.8.1b_2";
+	@Override
+	public String getVersion() {
+		return "1.2 (MC 1.0.0)";
 	}
 }
