bc3d3f6e5dfcf48dcf234eb51072f292ababfcf5
FloSoft <sf-team@siedler25.org>
2011-11-25 08:48:35 +0100

    updated readme added license

--- README.txt
+++ README.txt
@@ -1,9 +1,9 @@
 Editable Signs
 -------------------------------------------------------------------------------
 
-Current Version 1.8.1b_2
+Current Version 1.2
 
-for Minecraft Beta 1.8.1
+for Minecraft 1.0.0
 
 Depends on: ModLoader
 
@@ -16,6 +16,10 @@ Everything without changing one line of code in the original minecraft.jar
 -----------------------------
 
 Changelog:
+
+1.2 - Update
+- updated for Minecraft 1.0.0
+- added license
 
 1.8.1b_2 - Fix
 - fixed signPost/signWall Obfuscation
--- src/BlockEditableSign.java
+++ src/BlockEditableSign.java
@@ -1,3 +1,15 @@
+/** 
+ * Copyright (C) 2011 Flow86
+ * 
+ * AdditionalBuildcraftObjects is open-source.
+ *
+ * It is distributed under the terms of my Open Source License. 
+ * It grants rights to read, modify, compile or run the code. 
+ * It does *NOT* grant the right to redistribute this software or its 
+ * modifications in any form, binary or source, except if expressively
+ * granted by the copyright holder.
+ */
+
 /**
  * 
  */
@@ -13,6 +25,7 @@ public class BlockEditableSign extends BlockSign {
 		super(i, class1, flag);
 	}
 
+	@Override
 	public boolean blockActivated(World world, int i, int j, int k, EntityPlayer entityplayer) {
 		TileEntitySign tileentitysign = (TileEntitySign) world.getBlockTileEntity(i, j, k);
 		if (tileentitysign != null) {
